6e9a4c0f12f00e53d40931511811c7f26fe9e753
r1anl3 <levu1409@gmail.com>
2024-05-30 20:21:14 +0700

    modify code

--- csi-and-cv/collect.sh
+++ csi-and-cv/collect.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 SESS=$1
 
+if [ $# -eq 0 ]; then
+    SESS=1
+fi
+
+echo "Session: $0"
+
 # Collects the data from the CSI and CV experiments
 ./csi_frame_collect.py > sess_${SESS}_csi.csv &
 CSI_PID=$!
