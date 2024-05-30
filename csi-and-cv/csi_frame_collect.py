b186d87e53d3dcb6218f0d1a56903777399a7f89
r1anl3 <levu1409@gmail.com>
2024-05-30 19:36:00 +0700

    modify script

--- csi-and-cv/csi_frame_collect.py
+++ csi-and-cv/csi_frame_collect.py
@@ -1,124 +1,63 @@
+#!/usr/bin/python3
 import serial
 import time
-# import math
-# import numpy as np
-# import pandas as pd
 
-# import pycuda.driver as cuda
-# import pycuda.autoinit
-# import tensorrt as trt
-# import paho.mqtt.client as paho
-# from paho import mqtt
+# def inference(engine, h_input, d_input, h_output, d_output, input_data):
+#     stream = cuda.Stream()
+#     input_data = np.array(input_data)
+#     input_data /= 255
+#     # Set the input data (dummy data in this example)
+#     np.random.seed(123)
+#     #input_data = np.random.rand(*h_input.shape).astype(np.float32)
+#     np.copyto(h_input, input_data.ravel())
 
-# Function to load TensorRT engine from a '.engine' file
-def load_engine(engine_file_path):
-    with open(engine_file_path, 'rb') as f, trt.Runtime(trt.Logger(trt.Logger.INFO)) as runtime:
-        engine_data = f.read()
-        engine = runtime.deserialize_cuda_engine(engine_data)
-    return engine
+#     # Copy input data to the device
+#     cuda.memcpy_htod_async(d_input, h_input, stream)
 
-def allocate_buffers(engine):
-    h_input = cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(0)), dtype=np.float32)
-    h_output = cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(1)), dtype=np.float32)
-
-    d_input = cuda.mem_alloc(h_input.nbytes)
-    d_output = cuda.mem_alloc(h_output.nbytes)
-
-    return h_input, d_input, h_output, d_output
-
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s." % rc)
-
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
-
-def pub_mqtt(num):
-    client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
-    client.on_connect = on_connect
-
-# enable TLS for secure connection
-    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-    client.username_pw_set("JetsonNano", "JetsonNano123")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-    client.connect("1904448cf01a4564947dae8e889f5fee.s2.eu.hivemq.cloud", 8883)
-    client.loop_start()
-
-    client.on_publish = on_publish
-    print("MQTT called",num)
-    client.publish("predict", payload==str(num), qos=1)
-
-    client.loop_stop()
-    client.disconnect()
-
-def inference(engine, h_input, d_input, h_output, d_output, input_data):
-    stream = cuda.Stream()
-    input_data = np.array(input_data)
-    input_data /= 255
-    # Set the input data (dummy data in this example)
-    np.random.seed(123)
-    #input_data = np.random.rand(*h_input.shape).astype(np.float32)
-    np.copyto(h_input, input_data.ravel())
-
-    # Copy input data to the device
-    cuda.memcpy_htod_async(d_input, h_input, stream)
-
-    # Run inference
-    with engine.create_execution_context() as context:
-        context.execute_async(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
+#     # Run inference
+#     with engine.create_execution_context() as context:
+#         context.execute_async(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
     
-    # Copy output data to the host
-    cuda.memcpy_dtoh_async(h_output, d_output, stream)
-    stream.synchronize()
+#     # Copy output data to the host
+#     cuda.memcpy_dtoh_async(h_output, d_output, stream)
+#     stream.synchronize()
 
-    return h_output
+#     return h_output
 
-def process_data(data):
-    # Add your data processing logic here
-    print(data)
+# def process_data(data):
+#     # Add your data processing logic here
+#     print(data)
 
-def process(res):
-    # Parser
-    all_data = res.split(',')
-    csi_data = all_data[25].split(" ")
-    csi_data[0] = csi_data[0].replace("[", "")
-    csi_data[-1] = csi_data[-1].replace("]", "")
+# def process(res):
+#     # Parser
+#     all_data = res.split(',')
+#     csi_data = all_data[25].split(" ")
+#     csi_data[0] = csi_data[0].replace("[", "")
+#     csi_data[-1] = csi_data[-1].replace("]", "")
 
-    csi_data.pop()
-    csi_data = [int(c) for c in csi_data if c]
-    imaginary = []
-    real = []
-    for i, val in enumerate(csi_data):
-        if i % 2 == 0:
-            imaginary.append(val)
-        else:
-            real.append(val)
+#     csi_data.pop()
+#     csi_data = [int(c) for c in csi_data if c]
+#     imaginary = []
+#     real = []
+#     for i, val in enumerate(csi_data):
+#         if i % 2 == 0:
+#             imaginary.append(val)
+#         else:
+#             real.append(val)
 
-    csi_size = len(csi_data)
-    amplitudes = []
-    if len(imaginary) > 0 and len(real) > 0:
-        for j in range(int(csi_size / 2)):
-            amplitude_calc = math.sqrt(imaginary[j] ** 2 + real[j] ** 2)
-            amplitudes.append(amplitude_calc)
-    df = pd.DataFrame(amplitudes)
-    return df
-
-def predict(df):
-    columns_to_drop = [2,3,4,5,32,59,60,61,62]
-    df.drop(df.columns[columns_to_drop], axis=1,inplace=True)
-    #df = df.transpose().values.reshape((200, 55, 1))
-    output_result = inference(engine, h_input, d_input, h_output, d_output,df)
-    predicted_class = np.argmax(output_result)
-
-    print("Inference Result:")
-    print(predicted_class)
-    pub_mqtt(predicted_class)
+#     csi_size = len(csi_data)
+#     amplitudes = []
+#     if len(imaginary) > 0 and len(real) > 0:
+#         for j in range(int(csi_size / 2)):
+#             amplitude_calc = math.sqrt(imaginary[j] ** 2 + real[j] ** 2)
+#             amplitudes.append(amplitude_calc)
+#     df = pd.DataFrame(amplitudes)
+#     return df
 
 def main():
     serial_port = "/dev/ttyUSB0"  # Use the specified serial port
     baud_rate = 921600  # Set the baud rate to 921600
-    count = 0
+    # count = 0
     # Configure the serial port
     ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=0.1)
     dfs = []
