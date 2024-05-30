67c69e954cf25972f60fe9ff3e253e6a5f4effce
r1anl3 <levu1409@gmail.com>
2024-05-30 13:51:38 +0700

    test kill subprocess

--- csi-and-cv/csi_frame_collect.sh
+++ csi-and-cv/csi_frame_collect.sh
@@ -6,6 +6,10 @@ SES=s1
 # Trap termination signal (SIGINT)
 #trap 'kill -INT %1; exit' SIGINT
 
+function killSubproc(){
+    kill $(jobs -p -r)
+}
+
 # Run minicom with capture and filtering
 sudo minicom -b $BAURATE -D $PORT | grep "CSI_DATA" | perl -ne 'print time(),",$_"' > frame-csi-$SES.csv &
 
@@ -14,7 +18,7 @@ echo $MINICOM_PID
 
 sleep 10
 echo -ne "\x01x\r"
-kill -INT $MINICOM_PID
+trap killSubproc INT
 
 # Exit script
 exit 0
