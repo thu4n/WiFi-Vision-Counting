35f7ba8428fedabfe0393c6282ca294673946091
show <levu1409@gmail.com>
2024-06-05 13:07:12 +0700

    add new file

---
+++ csi-and-cv/csi_dev_0.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import serial
+import time
+import os
+
+def main():
+    serial_port = "/dev/ttyUSB0"  # Use the specified serial port
+    baud_rate = 921600  # Set the baud rate to 921600
+    # Configure the serial port
+    ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=0.1)
+    try:
+        duration = 10
+        frame_count = 0
+        start_time = time.time()
+        while True:
+            try:
+                data = ser.readline().decode("utf-8").strip()
+                if "CSI_DATA" in data:
+                    print(data.strip() + "," + str(time.time()))
+                    frame_count += 1
+                if time.time() - start_time > duration:
+                    print(f"Frames: {frame_count-1}")
+                    break
+                        
+            except Exception as e:
+                print("Error:",{e})
+                pass
+
+    except KeyboardInterrupt:
+        print("Exiting gracefully.")
+    finally:
+        ser.close()
+
+if __name__ == "__main__":
+    main()
