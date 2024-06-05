4618e9a28efd09c9cb5ea336d789f14043d61728
r1anl3 <levu1409@gmail.com>
2024-06-05 14:16:13 +0700

    feat: Get duration from input

--- csi-and-cv/collect.sh
+++ csi-and-cv/collect.sh
@@ -3,6 +3,7 @@ SESS=$1
 DEV_0="csi_dev_0"
 DEV_1="csi_dev_1"
 
+# Check if the directories exist, if not create them
 if [ ! -d "$DEV_0" ]; then
     mkdir $DEV_0
 fi
@@ -11,27 +12,38 @@ if [ ! -d "$DEV_1" ]; then
     mkdir $DEV_1
 fi
 
+# Check if the session number is provided, if not set it to 1
 if [ $# -eq 0 ]; then
     SESS=1
 fi
 
 echo "Session: $SESS"
 
+# Get the duration of the experiment
+read "Duration(seconds): " DURATION
+
+# Check if the duration is provided, if not set it to 10 seconds
+if [ -z "$DURATION" ]; then
+    DURATION=10
+fi
+
+# Get the password for sudo
 read -s -p "Enter Password for sudo: " PASSWORD
+
+# Check if the password is correct
 echo $PASSWORD | sudo -S echo "Password accepted"
 
 # Collects the data from the CSI and CV experiments
-sudo python3 csi_frame_collect.py 0 > ./$DEV_0/sess_${SESS}_csi.csv &
-sudo python3 csi_frame_collect.py 1 > ./$DEV_1/sess_${SESS}_csi.csv &
+sudo python3 csi_frame_collect.py 0 $DURATION > ./$DEV_0/sess_${SESS}_csi.csv &
+sudo python3 csi_frame_collect.py 1 $DURATION > ./$DEV_1/sess_${SESS}_csi.csv &
 
+# Check if the CSI experiment is running
 if [ $? -eq 0 ]; then
-    ./cv_frame_collect.py $SESS &
+    ./cv_frame_collect.py $SESS $DURATION &
 fi
-#CSI_PID=$!
-#CV_PID=$!
 
-#echo "CSI_PID: $CSI_PID"
-#echo "CV_PID: $CV_PID"
-
+# Wait for the experiment to finish
 wait
+
+# Exit script
 exit 0
