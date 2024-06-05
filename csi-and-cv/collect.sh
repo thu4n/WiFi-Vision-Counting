eb3e58064103ffde86434379c8c46f2516ccb2e8
r1anl3 <levu1409@gmail.com>
2024-06-05 13:11:28 +0700

    feat: Create folder for devices

--- csi-and-cv/collect.sh
+++ csi-and-cv/collect.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 SESS=$1
+DEV_0="csi_dev_0"
+DEV_1="csi_dev_1"
+
+if [ ! -d "$DEV_0" ]; then
+    mkdir $DEV_0
+fi
+
+if [ ! -d "$DEV_1" ]; then
+    mkdir $DEV_1
+fi
 
 if [ $# -eq 0 ]; then
     SESS=1
@@ -8,8 +18,8 @@ fi
 echo "Session: $SESS"
 
 # Collects the data from the CSI and CV experiments
-sudo python3 csi_dev_0.py > ./csi_dev_0/sess_${SESS}_csi.csv &
-sudo python3 csi_dev_0.py > ./csi_dev_1/sess_${SESS}_csi.csv &
+sudo python3 csi_dev_0.py > ./$DEV_0/sess_${SESS}_csi.csv &
+sudo python3 csi_dev_0.py > ./$DEV_1/sess_${SESS}_csi.csv &
 #CSI_PID=$!
 ./cv_frame_collect.py $SESS &
 #CV_PID=$!
