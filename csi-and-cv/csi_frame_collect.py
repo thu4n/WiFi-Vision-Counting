7a5d1de4f2d6279cb00cf987d4e4c912b8b07256
r1anl3 <levu1409@gmail.com>
2024-07-22 13:11:11 +0700

    modify script for Windows

--- csi-and-cv/csi_frame_collect.py
+++ csi-and-cv/csi_frame_collect.py
@@ -4,33 +4,33 @@ import time
 import sys
 
 def main():
-    dev= sys.argv[1] if len(sys.argv) > 0 else 0
-    serial_port = f"/dev/ttyUSB{dev}"  # Use the specified serial port
-    baud_rate = 921600  # Set the baud rate to 921600
-    # Configure the serial port
-    ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=0.1)
+    dev= sys.argv[1]  # Get the serial port number from the command line
+    if not dev.isdigit():
+        print("Invalid serial port number")
+        sys.exit(1)
+    serial_port = f"COM{dev}"  # Set the serial port number
+    baud_rate = 921600  # Set the baud rate
+    ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=0.1) # Configure the serial port
+    header = "type,role,mac,rssi,rate,sig_mode,mcs,bandwidth,smoothing,not_sounding,aggregation,stbc,fec_coding,sgi,noise_floor,ampdu_cnt,channel,secondary_channel,local_timestamp,ant,sig_len,rx_state,real_time_set,real_timestamp,len,CSI_DATA,machine_timestamp"
+    duration = 10 # Set the duration 
     try:
-        duration = int(sys.argv[2])  # capture duration in seconds
-        frame_count = 0
-        start_time = time.time()
+        start_time = time.time() # Get the current time
+        csi_file = open("csi_data.csv", "w") # Open the file to write the CSI data
+        csi_file.write(header + "\n") # Write the header to the file
         while True:
             try:
-                data = ser.readline().decode("utf-8").strip()
-                if data.startswith("CSI_DATA") and data.endswith("]"):
-                    print(data.strip() + "," + str(time.time()))
-                    frame_count += 1
-                if time.time() - start_time > duration:
-                    print(f"Frames: {frame_count-1}")
+                if time.time() - start_time >= duration:
                     break
-                        
+                data = ser.readline().decode("utf-8").strip()
+                if data.startswith("CSI_DATA") and data.endswith("]"): # Check if the data is a CSI frame
+                    csi_file.write(data + "," + str(time.time()) + "\n")
             except Exception as e:
                 print("Error:",{e})
                 pass
-
     except KeyboardInterrupt:
         print("Exiting gracefully.")
     finally:
-        ser.close()
-
+        ser.close() # Close the serial port
+        csi_file.close() # Close the file
 if __name__ == "__main__":
     main()
